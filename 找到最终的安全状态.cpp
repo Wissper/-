080d62fdc17e52c00d729abe66fc86f0f21cac1e
Whisper <wy288170679@163.com>
2022-06-18 19:55:34 +0800

    修复setflag()函数在过程中即将flag改为1的bug

--- 找到最终的安全状态.cpp
+++ 找到最终的安全状态.cpp
@@ -2,48 +2,39 @@
 #include <vector>
 using namespace std;
 
-int setflag(vector<vector<int>>& graph, vector<int>& flag, int i);
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int size = graph.size();
+        vector<int> flag(size, 0);
+        for(int i=0; i<size; i++){
+            if(graph[i].size() == 0)
+                flag[i] = 1;
+        }
+        vector<int> ans;
+        for(int i=0; i<size; i++){
+            int res = setflag(graph, flag, i);
+            if(res == 1){
+                ans.push_back(i);
+            }
+        }
+        //for(int i=0; i<size; i++) cout<<flag[i]<<endl;
+        return ans;
+    }
 
-int main(){
-	vector<vector<int>> graph;
-	cin >> graph;
-	
-	int size = graph.size();
-    vector<int> flag(size, 0);
-    for(int i=0; i<size; i++){
-        if(graph[i].size() == 0)
-            flag[i] = 1;
-    }
-    
-    vector<int> ans;
-    for(int i=0; i<size; i++){
-        int res = setflag(graph, flag, i);
-        if(res == 1){
-            ans.push_back(i);
+    int setflag(vector<vector<int>>& graph, vector<int>& flag, int i){
+        if(flag[i] != 0) return flag[i];
+        flag[i] = -1;
+        bool f = false;
+        for(int j=0; j<graph[i].size(); j++){
+            int res = setflag(graph, flag, graph[i][j]);
+            if(res == -1){
+                f = true;
+                flag[i] = -1;
+                break;
+            }
         }
+        if(!f) flag[i] = 1;
+        return flag[i];
     }
-    
-    for(int i=0; i<ans.size(); i++){
-    	cout << ans[i];
-    	if(i != ans.size() - 1) cout << ",";
-	}
-	cout << endl;
-	
-	return 0;
-}
-
-int setflag(vector<vector<int>>& graph, vector<int>& flag, int i){
-    if(flag[i] != 0) return flag[i];
-    flag[i] = -1;
-    for(int j=0; j<graph[i].size(); j++){
-        int res = setflag(graph, flag, graph[i][j]);
-        if(res == -1){
-            flag[i] = -1;
-            break;
-        }
-        else{
-            flag[i] = 1;
-        }
-    }
-    return flag[i];
-}
+};
