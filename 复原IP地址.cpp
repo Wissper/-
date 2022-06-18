fd62f967e2874e4e4f9e8f5be0579b19b0736bac
Whisper <wy288170679@163.com>
2022-06-18 18:43:23 +0800

    实现深度优先算法函数，并完成总体框架

--- 复原IP地址.cpp
+++ 复原IP地址.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using namespace std;
 
+vector<string> parse(string s, int step);
 
 int main(void) {
 	string s;
@@ -16,5 +17,47 @@ int main(void) {
 		return 0;
 	}
 
-
+	ans = parse(s, 1);
+	
+	cout << "[";
+    
+    for(int i=0; i<ans.size(); i++){
+    	cout << ans[i];
+    	if(i != ans.size()-1) cout << ", ";
+	}
+	
+	cout << "]" << endl;
+	
+	return 0;
 }
+
+
+vector<string> parse(string s, int step){
+    vector<string> ans;
+    if(s.length() == 0) return ans;
+    if(step == 4){
+        if(s.length() > 3 || (s[0]=='0' && s.length() > 1)){
+            return ans;
+        }
+        else{
+            ans.push_back(s);
+            return ans;
+        }
+    }
+    string pref = "";
+    int pre = 0;
+    for(int i=0; i<3; i++){
+        if(s.length() == 0) return ans;
+        pref += s[0];
+        pre = pre * 10 + s[0] - '0';
+        if(pre > 255) break;
+        s.erase(s.begin());
+        vector<string>res = parse(s, step + 1);
+        for(int j=0; j<res.size(); j++){
+            ans.push_back(pref+"."+res[j]);
+        }
+        if(pre == 0) break;
+    }
+    return ans;
+}
+
