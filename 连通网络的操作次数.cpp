710455228a2c2756d1fbbddb2d51309f4889096e
Whisper <wy288170679@163.com>
2022-07-04 15:30:16 +0800

    首先进行条件判断

---
+++ 连通网络的操作次数.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int num = connections.size();
+        if(num < n-1) return -1;
+        
+    }
+};
